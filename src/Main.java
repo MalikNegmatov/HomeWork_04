2650180e7fbba721e07665a78d7b284272764e4a
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-12 12:45:14 +0300

    Инициализация проекта. Домашнее задание.

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+             }
+
+    public static void task1_1 () {
+    }
+    public static void task1 () {
+        System.out.println("Задача №1");
+        // Пишем код для задачи 1
+        task1_1();
+    }
+
+    public static void task2_1 () {
+    }
+    public static void task2 () {
+        System.out.println("\nЗадача №2");
+        // Пишем код для задачи 2
+        task2_1();
+        }
+}
