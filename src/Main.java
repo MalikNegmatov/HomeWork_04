4464d10abbf46d65f1c7d0786f162f8aec8d6c56
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-12 16:09:14 +0300

    Добавили решение задачи 1

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,22 @@ public class Main {
         task2();
              }
 
-    public static void task1_1 () {
+    public static void task1_1 (byte osVersion) {
+
+          if (osVersion == 1) {
+            System.out.println("Вы используете Android." +
+            " Установите версию приложения для Android по ссылке Link1.");
+          }
+          else {
+              System.out.println("Вы используете iOS." +
+                      " Установите версию приложения для iOS по ссылке Link2.");
+          }
     }
     public static void task1 () {
         System.out.println("Задача №1");
         // Пишем код для задачи 1
-        task1_1();
+        task1_1((byte) 1);
+        task1_1((byte) 0);
     }
 
     public static void task2_1 () {
